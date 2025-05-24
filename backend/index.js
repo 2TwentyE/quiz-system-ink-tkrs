fc9c998d6194a058c71480970b97c7ca340fb224
Daniil Kostromin <danil.kostromin.99@mail.ru>
2025-05-24 15:38:25 +0800

    debug email

--- backend/index.js
+++ backend/index.js
@@ -11,10 +11,6 @@ const resultRoutes = require('./routes/results');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-pool.query('SELECT NOW()')
-    .then(res => console.log('Успешное подключение к БД', res.rows[0]))
-    .catch(err => console.error('Ошибка подключения к БД', err));
-
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -24,3 +20,7 @@ app.use('/api/results', resultRoutes);
 app.get('/', (req, res) => res.send('Сервер работает'));
 
 app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
+
+pool.query('SELECT NOW()')
+    .then(res => console.log('Успешное подключение к БД', res.rows[0]))
+    .catch(err => console.error('Ошибка подключения к БД', err));
