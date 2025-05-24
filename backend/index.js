ddb2a68bfe91d0021e311df2733ba00c7ff6a492
Daniil Kostromin <danil.kostromin.99@mail.ru>
2025-05-24 11:53:40 +0800

    connect pg

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
 const express = require('express');
+const pool = require('./db');
+
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
@@ -7,6 +10,10 @@ const resultRoutes = require('./routes/results');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+pool.query('SELECT NOW()')
+    .then(res => console.log('Успешное подключение к БД', res.rows[0]))
+    .catch(err => console.error('Ошибка подключения к БД', err));
 
 app.use(cors());
 app.use(bodyParser.json());
