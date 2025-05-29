f79af386c2bcbaa173409942d17b3054455acc84
2TwentyE <154894556+2TwentyE@users.noreply.github.com>
2025-05-29 08:39:56 +0000

    debug post json file

--- public/app.js
+++ public/app.js
@@ -243,7 +243,9 @@ async function registerUser(name, email = '') {
         const response = await fetch('https://backend-production-5f60.up.railway.app/api/users/register', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ name, email })
+            body: JSON.stringify({ 
+                name: '', 
+                email: '' })
         });
         const data = await response.json();
         if (response.ok) {
